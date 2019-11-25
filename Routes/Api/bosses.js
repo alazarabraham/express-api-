110e16390ee2731efa59b61e3faf29de28385289
Alazar Markos <alazarabraham1@gmail.com>
2019-11-24 21:38:50 -0500

    first commit

--- Routes/Api/bosses.js
+++ Routes/Api/bosses.js
@@ -1,6 +1,7 @@
 const express =require("express");
 const router = express.Router();
 const bosses = require("../../Model/bosses");
+const uuid = require("uuid")
 
 router.get('/', function(req,res){
     res.json(bosses);
@@ -11,6 +12,19 @@ router.get('/:id', (req,res) =>{
     if (found){
     res.json(bosses.filter(boss => boss.id === parseInt(req.params.id)))}
     else{res.status(400).json({msg:`No member with id of ${req.params.id}`})}
+})
+router.post('/',(req, res)=>{
+const newBosses = {
+    id: uuid.v4(),
+    name: req.body.name,
+    status: req.body.status,
+    richenough: req.body.richenough
+};
+if(!newBosses.name || !newBosses.status){
+    return res.status(400).json({msg: "please include name and status"})
+}
+bosses.push(newBosses);
+res.json(bosses)
 })
 
 module.exports = router;
